4670cd858215a764e4f965fedb9675762435d70a
Usmaan-Shahh <m.usmaanshahh@gmail.com>
2026-01-28 21:46:46 +0530

    refactor(auth): remove unnecessary blank line in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,7 +6,6 @@ import bcrypt from 'bcrypt';
 
 
 @Injectable() // Means TypeScript Emits Constructor Parameter Type metadata allowing NestJS to resolve  and inject the PrismaService instance into AuthService.
-
 export class AuthService {
 
     constructor(private readonly prisma: PrismaService) {}
