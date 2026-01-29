20193fd0fe4dc846bce07dc9678e5ed1bb23eb5f
Usmaan-Shahh <m.usmaanshahh@gmail.com>
2026-01-29 14:49:04 +0530

    fix: update UserRepository to ensure proper database communication and adjust TypeScript output directory

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -3,12 +3,14 @@ import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 
 @Injectable()
-export class UserRepository extends Repository<User> {
+export class UserRepository extends Repository<User> { //Our Custom Repository inherits all TypeORM built-in Repository Methods
+  
   constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+    //Without createEntityManager(), the repository cannot talk to the database.     
   }
 
-  async findByEmail(email: string): Promise<User | null> {
+  async findByEmail(email: string): Promise< User | null > {
     return this.findOne({
       where: { email },
     });
@@ -18,4 +20,5 @@ export class UserRepository extends Repository<User> {
     const user = this.create(data);
     return this.save(user);
   }
+
 }
