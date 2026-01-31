3661112da08eb9fa423a7f67c931f20d94cb2042
Usmaan-Shahh <m.usmaanshahh@gmail.com>
2026-01-31 20:28:25 +0530

    refactor(auth): format LoginDto and RegisterDto properties for improved readability; remove unused e2e test files

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,8 +2,10 @@ import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class LoginDto {
 
-  @IsEmail({}, { message: 'Invalid email format' }) email: string;
+  @IsEmail({}, { message: 'Invalid email format' })
+  email: string;
 
-  @IsNotEmpty() @MinLength(6, { message: 'Password must be at least 6 characters' }) password: string;
+  @IsNotEmpty() @MinLength(6, { message: 'Password must be at least 6 characters' }) 
+  password: string;
   
 }
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -3,9 +3,11 @@ import { IsEmail, IsNotEmpty, MinLength, MaxLength } from 'class-validator';
 
 export class RegisterDto {
 
-  @IsEmail({}, { message: 'Invalid Email Format' }) email: string;
+  @IsEmail({}, { message: 'Invalid Email Format' }) 
+  email: string;
 
-  @IsNotEmpty() @MinLength(6) @MaxLength(20) password: string;
+  @IsNotEmpty() @MinLength(6) @MaxLength(20) 
+  password: string;
   
 }
 
